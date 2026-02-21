a9b7e1f0784dd8cc55e2bc31f449e3c58a3f94c8
Paulo Amorim <pauloamorim2006@hotmail.com>
2026-02-21 06:48:15 -0300

    Change program to .NET 9

--- Backend/src/SalesForce.Api/Program.cs
+++ Backend/src/SalesForce.Api/Program.cs
@@ -1,20 +1,46 @@
-using Microsoft.AspNetCore.Hosting;
+using Asp.Versioning.ApiExplorer;
+using AutoMapper;
+using ERP.Api.Configuration;
+using ERP.Data.Context;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using SalesForce.Api.Configuration;
+using System.Reflection;
 
-namespace ERP.Api
+var builder = WebApplication.CreateBuilder(args);
+
+// Configure Services
+builder.Services.AddDbContext<SalesForceDbContext>(options =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
-}
+builder.Services.AddIdentityConfig(builder.Configuration);
+
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+builder.Services.CacheConfiguration(builder.Configuration);
+
+builder.Services.LoggerConfiguration();
+
+builder.Services.AddApiConfig();
+
+builder.Services.AddSwaggerConfig();
+
+builder.Services.HealthCheckConfiguration(builder.Configuration);
+
+builder.Services.ResolveDependencies();
+
+var app = builder.Build();
+
+// Configure Pipeline
+var apiVersionDescriptionProvider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+
+app.UseApiConfig(app.Environment, builder.Configuration);
+
+app.UseSwaggerConfig(apiVersionDescriptionProvider);
+
+app.Run();
