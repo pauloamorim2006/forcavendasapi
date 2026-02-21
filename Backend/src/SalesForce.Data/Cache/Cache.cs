dc8bcadbb14d97a09ccd7e108a4bd66b47c8b195
Paulo Amorim <pauloamorim2006@hotmail.com>
2026-02-21 06:39:33 -0300

    Change cache

--- Backend/src/SalesForce.Data/Cache/Cache.cs
+++ Backend/src/SalesForce.Data/Cache/Cache.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Text.Json;
@@ -9,17 +10,22 @@ namespace SalesForce.Data.Cache
     public class Cache : ICache
     {
         private readonly IDistributedCache _distributedCache;
+        private readonly bool _enableCache;
 
-        public Cache(IDistributedCache distributedCache)
+        public Cache(IDistributedCache distributedCache, IConfiguration configuration)
         {
             _distributedCache = distributedCache;
+            _enableCache = configuration.GetValue<bool>("CacheConfig:EnableCache");
         }
 
         public async Task<List<T>> GetListAsync<T>(string key)
         {
-            var keyValue = $"{nameof(T)}.{key}";
             var list = new List<T>();
-            var json = await _distributedCache.GetStringAsync(key);            
+
+            if (!_enableCache) return list;
+
+            var keyValue = $"{nameof(T)}.{key}";
+            var json = await _distributedCache.GetStringAsync(keyValue);            
             if (json != null)
             {
                 list = JsonSerializer.Deserialize<List<T>>(json);
@@ -29,6 +35,8 @@ namespace SalesForce.Data.Cache
 
         public async Task SetListAsync<T>(string key, IEnumerable<T> data)
         {
+            if (!_enableCache) return;
+
             DistributedCacheEntryOptions cacheOptions = new DistributedCacheEntryOptions();
             cacheOptions.SetAbsoluteExpiration(TimeSpan.FromMinutes(1));
             var keyValue = $"{nameof(T)}.{key}";
